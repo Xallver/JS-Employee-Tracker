fb7601d69c3f40ce433338ad891ac5eaac1d7c94
Xallver <jamesstanley11@gmail.com>
2022-04-29 22:16:41 -0400

    created function addRole & addNewRole

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ db.connect(function (error) {
     if (error) throw error;
     console.log("Welcome to Employee Manager");
 
+    db.query("SELECT * from role", function (error, res) {
+        roles = res.map(role => ({
+            name: role.title,
+            value: role.id
+        }))
+    })
+    db.query("SELECT * from department", function (error, res) {
+        departments = res.map(dep => ({
+            name: dep.name,
+            value: dep.id
+        }))
+    })
+    db.query("SELECT * from employee", function (error, res) {
+        employees = res.map(emp => ({
+            name: `${emp.first_name} ${emp.last_name}`,
+            value: emp.id
+        }))
+    })
+
     inquirerPrompt();
 });
 
@@ -93,10 +112,6 @@ function mainMenu(options) {
 
 function viewAllDepartments() {
     db.query("SELECT * FROM department", function (error, res) {
-        departments = res.map(dep => ({
-            name: dep.name,
-            value: dep.id
-        }))
         console.table(res);
         endOrMain();
     });
@@ -104,10 +119,6 @@ function viewAllDepartments() {
 
 function viewAllRoles() {
     db.query("SELECT * FROM role", function (error, res) {
-        roles = res.map(role => ({
-            name: role.title,
-            value: role.id
-        }))
         console.table(res);
         endOrMain();
     });
@@ -115,10 +126,6 @@ function viewAllRoles() {
 
 function viewAllEmployees() {
     db.query("SELECT * FROM employee", function (error, res) {
-        employees = res.map(emp => ({
-            name: `${emp.first_name} ${emp.last_name}`,
-            value: emp.id
-        }))
         console.table(res);
         endOrMain();
     });
@@ -142,5 +149,40 @@ function newDepartment(data) {
         function (error, res) {
             if (error) throw error;
         });
+    endOrMain();
+};
+
+function addRole() {
+    inquirer.prompt([{
+                type: "input",
+                message: "What is the name of the new role?",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "What is the salary of the new role?",
+                name: "salary"
+
+            },
+            {
+                type: "list",
+                message: "Which department is the new role in?",
+                name: "id",
+                choices: departments
+            }
+        ])
+        .then(function (response) {
+            addNewRole(response);
+        });
+};
+
+function addNewRole(data) {
+    db.query("INSERT INTO role SET ?", {
+        title: data.title,
+        salary: data.salary,
+        department_id: data.id
+    }, function (error, res) {
+        if (error) throw error;
+    });
     endOrMain();
 };
